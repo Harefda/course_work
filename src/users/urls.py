e7f7cee078d3f5f6543a749cb7af40d71a771d4f
Harefda <harefda@mail.ru>
2022-05-26 00:01:34 +0300

    stuff

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from users.views import user_views, hockey_views
+from users.views import user_views, hockey_views, frontend_views
 
 
 urlpatterns = [
     path('create', user_views.create_user_api),
     path('login', user_views.authenticate_user_api),
     path('hockey_player/create', hockey_views.create_hockey_player),
-    path('test', hockey_views.get_6_best_players)
+    path('test', hockey_views.get_6_best_players),
+    path('', frontend_views.main)
 ]
