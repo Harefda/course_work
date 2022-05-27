670bcdffef8b8e99aed5f46a21b82eb525608dfc
Harefda <harefda@mail.ru>
2022-05-28 01:14:20 +0300

    made some fixes

--- src/app/views.py
+++ src/app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 
+from users.models import HockeyPlayer
+
 
 def main_page(request):
-    return render(request, "main.html")
+    hockey_players = HockeyPlayer.objects.all()
+    return render(request, "table.html", {"players": hockey_players})
 
 def login_page(request):
     return render(request, "login.html")
